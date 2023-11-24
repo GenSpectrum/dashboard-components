023cd398f0f8603ababb6b0cc0e7edf516ea11ab
Chaoran Chen <mail@chaoran-chen.de>
2023-11-25 00:00:17 +0100

    feat(components): add autocomplete/datalist to country filter

--- lit-components/src/country-filter.ts
+++ lit-components/src/country-filter.ts
@@ -1,5 +1,9 @@
 import { LitElement, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { Task } from '@lit/task';
+import { consume } from '@lit/context';
+import { lapisContext } from './lapis-context';
+import { FetchAggregatedQuery } from './query/FetchAggregatedQuery';
 
 /**
  * @fires country-changed - Indicates when the country changes
@@ -9,11 +13,38 @@ export class CountryFilter extends LitElement {
     @property()
     country = '';
 
+    @consume({ context: lapisContext })
+    lapis: string = '';
+
+    private fetchingTask = new Task(this, {
+        task: async ([lapis], { signal }) => {
+            const query = new FetchAggregatedQuery<{
+                country: string | null;
+            }>({}, ['country']);
+            const dataset = await query.evaluate(lapis, signal);
+            return dataset.content
+                .filter((row) => row.country !== null)
+                .map((row) => row.country!)
+                .sort();
+        },
+        args: () => [this.lapis],
+    });
+
     override render() {
-        return html`
-            <input type="text" .value="${this.country}" @input="${this.onInput}" />
-            <button @click="${this.submitCountry}">Submit</button>
-        `;
+        return this.fetchingTask.render({
+            pending: () => html`
+                <input type="text" .value="${this.country}" @input="${this.onInput}" disabled />
+                <button @click="${this.submitCountry}">Submit</button>
+            `,
+            complete: (countries) => html`
+                <input type="text" .value="${this.country}" @input="${this.onInput}" list="countries" />
+                <datalist id="countries">
+                    ${countries.map((country) => html`<option value="${country}"></option>`)}
+                </datalist>
+                <button @click="${this.submitCountry}">Submit</button>
+            `,
+            error: (e) => html`<p>Error: ${e}</p>`,
+        });
     }
 
     onInput(event: Event) {
