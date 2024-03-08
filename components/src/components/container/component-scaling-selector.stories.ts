51918829a25c09b111bbc0ab52cda202d4e7b96d
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-03-08 14:37:21 +0100

    test(components): implement a basic test for a component using Storybook #42

--- components/src/components/container/component-scaling-selector.stories.ts
+++ components/src/components/container/component-scaling-selector.stories.ts
@@ -1,14 +1,27 @@
+import { expect } from '@storybook/jest';
 import { html } from 'lit';
 import { Meta, StoryObj } from '@storybook/web-components';
 import './component-scaling-selector';
+import { userEvent, waitFor } from '@storybook/testing-library';
+import { withinShadowRoot } from '../../storybook/withinShadowRoot.story';
 
 const meta: Meta = {
     title: 'Component/Scaling Selector',
     component: 'gs-component-scaling-selector',
+    argTypes: {
+        setYAxisScaleType: { action: true },
+    },
 };
 
 export default meta;
 
 export const ScalingSelectorStory: StoryObj = {
-    render: () => html`<gs-component-scaling-selector />`,
+    render: ({ setYAxisScaleType }) => html`
+        <gs-component-scaling-selector .setYAxisScaleType=${setYAxisScaleType}></gs-component-scaling-selector>
+    `,
+    play: async ({ canvasElement, args }) => {
+        const canvas = await withinShadowRoot(canvasElement, 'gs-component-scaling-selector');
+        await userEvent.selectOptions(canvas.getByRole('combobox'), 'logit');
+        await waitFor(() => expect(args.setYAxisScaleType).toHaveBeenCalledWith('logit'));
+    },
 };
