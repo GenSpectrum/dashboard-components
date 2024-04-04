56b5208d6212fed293bc66036c993abe0f785001
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-04-04 16:06:38 +0200

    test(components): more tests for Temporal #26

--- components/src/utils/temporal.spec.ts
+++ components/src/utils/temporal.spec.ts
@@ -1,4 +1,4 @@
-import { expect, describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import {
     generateAllDaysInRange,
     generateAllMonthsInRange,
@@ -7,13 +7,13 @@ import {
     Year,
     YearMonth,
     YearMonthDay,
+    YearWeek,
 } from './temporal';
 
-// TODO The tests don't work: error: "TypeError: dayjs is not a function"
+const cache = TemporalCache.getInstance();
 
 describe('generateAllDaysInRange', () => {
     it('should return all days in range', () => {
-        const cache = TemporalCache.getInstance();
         expect(
             generateAllDaysInRange(YearMonthDay.parse('2020-01-01', cache), YearMonthDay.parse('2020-01-04', cache)),
         ).deep.equal([
@@ -27,7 +27,6 @@ describe('generateAllDaysInRange', () => {
 
 describe('generateAllMonthsInRange', () => {
     it('should return all months in range', () => {
-        const cache = TemporalCache.getInstance();
         expect(
             generateAllMonthsInRange(YearMonth.parse('2020-01', cache), YearMonth.parse('2020-04', cache)),
         ).deep.equal([
@@ -41,12 +40,57 @@ describe('generateAllMonthsInRange', () => {
 
 describe('generateAllYearsInRange', () => {
     it('should return all years in range', () => {
-        const cache = TemporalCache.getInstance();
         expect(generateAllYearsInRange(Year.parse('2020-01', cache), Year.parse('2023', cache))).deep.equal([
             Year.parse('2020', cache),
             Year.parse('2021', cache),
             Year.parse('2022', cache),
             Year.parse('2023', cache),
         ]);
+    });
+});
+
+describe('YearMonthDay', () => {
+    it('should parse from string', () => {
+        const underTest = YearMonthDay.parse('2020-01-01', cache);
+
+        expect(underTest.yearNumber).equal(2020);
+        expect(underTest.monthNumber).equal(1);
+        expect(underTest.dayNumber).equal(1);
+        // seems to be a bug in dayjs: https://github.com/iamkun/dayjs/issues/2620
+        expect(underTest.week.text).equal('2019-01');
+        expect(underTest.text).equal('2020-01-01');
+    });
+});
+
+describe('YearWeek', () => {
+    it('should parse from string', () => {
+        const underTest = YearWeek.parse('2020-02', cache);
+
+        expect(underTest.isoYearNumber).equal(2020);
+        expect(underTest.isoWeekNumber).equal(2);
+        expect(underTest.firstDay.text).equal('2020-01-06');
+        expect(underTest.text).equal('2020-02');
+    });
+});
+
+describe('YearMonth', () => {
+    it('should parse from string', () => {
+        const underTest = YearMonth.parse('2020-01', cache);
+
+        expect(underTest.yearNumber).equal(2020);
+        expect(underTest.monthNumber).equal(1);
+        expect(underTest.text).equal('2020-01');
+        expect(underTest.firstDay.text).equal('2020-01-01');
+    });
+});
+
+describe('Year', () => {
+    it('should parse from string', () => {
+        const underTest = Year.parse('2020', cache);
+
+        expect(underTest.year).equal(2020);
+        expect(underTest.text).equal('2020');
+        expect(underTest.firstDay.text).equal('2020-01-01');
+        expect(underTest.firstMonth.text).equal('2020-01');
     });
 });
