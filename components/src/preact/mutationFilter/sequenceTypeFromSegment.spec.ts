142c109f9b7907b343b274cf2a68fd62d40153a5
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-04-17 16:09:51 +0200

    fixup! feat(components): add mutation filter component #8

--- components/src/preact/mutationFilter/sequenceTypeFromSegment.spec.ts
+++ components/src/preact/mutationFilter/sequenceTypeFromSegment.spec.ts
@@ -45,22 +45,22 @@ describe('getSequenceType', () => {
         ],
     };
 
-    it('should return nucleotide, even when the segment is undefined for singe segmented genome', () => {
+    it('should return nucleotide when the segment is undefined for singe segmented genome', () => {
         expect(sequenceTypeFromSegment('nuc1', singleSegmentedReferenceGenome)).toBe('nucleotide');
         expect(sequenceTypeFromSegment(undefined, singleSegmentedReferenceGenome)).toBe('nucleotide');
     });
 
-    it('should return undefined, even when the segment is undefined for multi segmented genome', () => {
+    it('should return undefined when the segment is undefined for multi segmented genome', () => {
         expect(sequenceTypeFromSegment('nuc1', multiSegmentedReferenceGenome)).toBe('nucleotide');
         expect(sequenceTypeFromSegment(undefined, multiSegmentedReferenceGenome)).toBe(undefined);
     });
 
-    it('should return amino acid, when the segment is a gene', () => {
+    it('should return amino acid when the segment is a gene', () => {
         expect(sequenceTypeFromSegment('gene1', singleSegmentedReferenceGenome)).toBe('amino acid');
         expect(sequenceTypeFromSegment('gene2', singleSegmentedReferenceGenome)).toBe('amino acid');
     });
 
-    it('should return undefined, when the segment is not found in the reference genome', () => {
+    it('should return undefined when the segment is not found in the reference genome', () => {
         expect(sequenceTypeFromSegment('not-existing', singleSegmentedReferenceGenome)).toBe(undefined);
     });
 });
