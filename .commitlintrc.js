50e97704bf776a6de3eb818bb3ca89051668461c
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-04-17 09:56:59 +0200

    build(root): lint commit messages
    Refs: #147

---
+++ .commitlintrc.js
@@ -0,0 +1,24 @@
+import {RuleConfigSeverity} from "@commitlint/types";
+
+/**
+ * @type {import('@commitlint/types').UserConfig}
+ */
+const Configuration = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        "scope-empty": [RuleConfigSeverity.Error, "never"],
+        "scope-enum": [
+            RuleConfigSeverity.Error,
+            "always",
+            [
+                "components",
+                "examples",
+                "github-actions",
+                "root", // used by dependabot to update the root package.json
+            ]
+        ],
+        "body-max-line-length": [RuleConfigSeverity.Error, "always", 200],
+    }
+};
+
+export default Configuration;
