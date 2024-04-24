ef3c6ca4e81833ae59eab2992f9851c4cd5de3e5
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-04-24 15:40:31 +0200

    chore(components): make prettier ignore the CHANGELOG.md since it's autogenerated

---
+++ components/.prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+    printWidth: 120,
+    tabWidth: 4,
+    trailingComma: 'all',
+    semi: true,
+    jsxSingleQuote: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+};
+export default config;
