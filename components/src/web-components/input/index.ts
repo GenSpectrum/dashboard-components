51adefd029e3312f9805ad958b573ca2bd1b0252
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-04-24 15:15:55 +0200

    fix(components): export MutationFilterComponent

--- components/src/web-components/input/index.ts
+++ components/src/web-components/input/index.ts
@@ -1,3 +1,4 @@
 export { DateRangeSelectorComponent } from './date-range-selector-component';
 export { LocationFilterComponent } from './location-filter-component';
 export { TextInputComponent } from './text-input-component';
+export { MutationFilterComponent } from './mutation-filter-component';
