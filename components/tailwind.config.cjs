4439c783221865dbbcb5c259ee9e33ead903ad39
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-04-30 15:19:22 +0200

    feat(components): disable dark mode in daisyui
    and make the CHANGELOG unaffected by "npm run format"

--- components/tailwind.config.cjs
+++ components/tailwind.config.cjs
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['src/**/*.{ts,tsx,html}', 'index.html'],
     theme: {
         extend: {},
     },
     plugins: [require('daisyui')],
+    daisyui: {
+        themes: ['light'],
+    },
 };
