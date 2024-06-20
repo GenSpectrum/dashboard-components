4b4ff2ec4c9321895711cbc3367c764d95b10d9d
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-06-20 10:55:23 +0200

    refactor(root): rename repository to dashboards-components

--- components/src/web-components/input/gs-date-range-selector.tsx
+++ components/src/web-components/input/gs-date-range-selector.tsx
@@ -91,7 +91,7 @@ export class DateRangeSelectorComponent extends PreactLitAdapter {
     /**
      * The width of the component.
      *
-     * Visit https://genspectrum.github.io/dashboards/?path=/docs/components-size-of-components--docs for more information.
+     * Visit https://genspectrum.github.io/dashboards-components/?path=/docs/components-size-of-components--docs for more information.
      */
     @property({ type: String })
     width: string = '100%';
