f5d8db09e310c530a2b128d93b264bc23073ffc7
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-07-22 15:18:17 +0200

    fix(components): mutations over time: position tooltip using css (instead of floatingUi) for better performance

--- components/src/preact/components/tooltip.tsx
+++ components/src/preact/components/tooltip.tsx
@@ -1,27 +1,51 @@
-import { flip, offset, shift } from '@floating-ui/dom';
 import { type FunctionComponent } from 'preact';
-import { useRef } from 'preact/hooks';
 import { type JSXInternal } from 'preact/src/jsx';
 
-import { dropdownClass } from './dropdown';
-import { useFloatingUi } from '../shared/floating-ui/hooks';
+export type TooltipPosition =
+    | 'top'
+    | 'top-start'
+    | 'top-end'
+    | 'bottom'
+    | 'bottom-start'
+    | 'bottom-end'
+    | 'left'
+    | 'right';
 
 export type TooltipProps = {
     content: string | JSXInternal.Element;
+    position?: TooltipPosition;
 };
 
-const Tooltip: FunctionComponent<TooltipProps> = ({ children, content }) => {
-    const referenceRef = useRef<HTMLDivElement>(null);
-    const floatingRef = useRef<HTMLDivElement>(null);
+function getPositionCss(position?: TooltipPosition) {
+    switch (position) {
+        case 'top':
+            return 'bottom-full translate-x-[-50%] left-1/2 mb-1';
+        case 'top-start':
+            return 'bottom-full mr-1 mb-1';
+        case 'top-end':
+            return 'bottom-full right-0 ml-1 mb-1';
+        case 'bottom':
+            return 'top-full translate-x-[-50%] left-1/2 mt-1';
+        case 'bottom-start':
+            return 'mr-1 mt-1';
+        case 'bottom-end':
+            return 'right-0 ml-1 mt-1';
+        case 'left':
+            return 'right-full translate-y-[-50%] top-1/2 mr-1';
+        case 'right':
+            return 'left-full translate-y-[-50%] top-1/2 ml-1';
+        case undefined:
+            return '';
+    }
+}
 
-    useFloatingUi(referenceRef, floatingRef, [offset(5), shift(), flip()]);
-
+const Tooltip: FunctionComponent<TooltipProps> = ({ children, content, position = 'bottom' }) => {
     return (
         <div className='relative'>
-            <div className='peer' ref={referenceRef}>
-                {children}
-            </div>
-            <div ref={floatingRef} className={`${dropdownClass} hidden peer-hover:block`}>
+            <div className='peer'>{children}</div>
+            <div
+                className={`absolute z-10 w-max bg-white p-4 border border-gray-200 rounded-md invisible peer-hover:visible ${getPositionCss(position)}`}
+            >
                 {content}
             </div>
         </div>
