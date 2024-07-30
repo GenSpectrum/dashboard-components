f29877fe655c9df7406bd3c297338b7c12409911
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-07-30 11:04:09 +0200

    feat(components): add select all/none button to dropdown selector #361

--- components/src/preact/components/checkbox-selector.tsx
+++ components/src/preact/components/checkbox-selector.tsx
@@ -18,6 +18,25 @@ export const CheckboxSelector = <Item extends CheckboxItem>({
 }: CheckboxSelectorProps<Item>) => {
     return (
         <Dropdown buttonTitle={label} placement={'bottom-start'}>
+            <button
+                className='btn btn-xs btn-ghost'
+                onClick={() => {
+                    const newItems = items.map((item) => ({ ...item, checked: true }));
+                    setItems(newItems);
+                }}
+            >
+                Select all
+            </button>
+            <button
+                className='btn btn-xs btn-ghost'
+                onClick={() => {
+                    const newItems = items.map((item) => ({ ...item, checked: false }));
+                    setItems(newItems);
+                }}
+            >
+                Select none
+            </button>
+            <div className='divider mt-0 mb-0' />
             <ul>
                 {items.map((item, index) => (
                     <li className='flex flex-row items-center' key={item.label}>
