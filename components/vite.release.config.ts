eed171091367ea3eb9f2178ea820743dc3c3a3b7
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-07-31 13:25:13 +0200

    fix(components): don't bundle dependencies, publish separate standalone bundle instead
    closes #407

--- components/vite.release.config.ts
+++ components/vite.release.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import dtsPlugin from 'vite-plugin-dts';
 
+import packageJson from './package.json';
+
 export default defineConfig({
     build: {
         lib: {
@@ -11,6 +13,9 @@ export default defineConfig({
         },
         sourcemap: true,
         minify: false,
+        rollupOptions: {
+            external: Object.keys(packageJson.dependencies),
+        },
     },
     plugins: [dtsPlugin({ rollupTypes: true })],
 });
