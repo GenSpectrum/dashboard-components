47b275b45aa98d17187725ece784133a91dd078e
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-08-01 17:48:39 +0200

    fix(components): reduce flashiness of loading skeletons
    closes #292

--- components/src/preact/components/loading-display.tsx
+++ components/src/preact/components/loading-display.tsx
@@ -1,5 +1,12 @@
 import { type FunctionComponent } from 'preact';
 
 export const LoadingDisplay: FunctionComponent = () => {
-    return <div aria-label={'Loading'} className='h-full w-full skeleton' />;
+    return (
+        <div
+            aria-label={'Loading'}
+            className='h-full w-full border-2 border-gray-100 rounded-md flex justify-center items-center'
+        >
+            <div className='loading loading-spinner loading-md text-neutral-500' />
+        </div>
+    );
 };
