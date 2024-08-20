79b307bbfd8cef11f2becde882f453b9dc142e75
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-08-20 17:02:35 +0200

    feat(components): mutations over time: change label of proportion selector #409

--- components/src/preact/components/proportion-selector-dropdown.stories.tsx
+++ components/src/preact/components/proportion-selector-dropdown.stories.tsx
@@ -17,7 +17,8 @@ export default meta;
 const WrapperWithState: FunctionComponent<{
     setMinProportion: (value: number) => void;
     setMaxProportion: (value: number) => void;
-}> = ({ setMinProportion, setMaxProportion }) => {
+    labelPrefix?: string;
+}> = ({ setMinProportion, setMaxProportion, labelPrefix }) => {
     const [proportionInterval, setProportionInterval] = useState({ min: 0.05, max: 1 });
 
     return (
@@ -31,24 +32,32 @@ const WrapperWithState: FunctionComponent<{
                 setProportionInterval({ ...proportionInterval, max: value });
                 setMaxProportion(value);
             }}
+            labelPrefix={labelPrefix}
         />
     );
 };
 
 export const ProportionSelectorStory: StoryObj<ProportionSelectorDropdownProps> = {
     render: (args) => {
-        return <WrapperWithState setMinProportion={args.setMinProportion} setMaxProportion={args.setMaxProportion} />;
+        return (
+            <WrapperWithState
+                setMinProportion={args.setMinProportion}
+                setMaxProportion={args.setMaxProportion}
+                labelPrefix={args.labelPrefix}
+            />
+        );
     },
     args: {
         setMinProportion: fn(),
         setMaxProportion: fn(),
+        labelPrefix: 'TestPrefix',
     },
     play: async ({ canvasElement, step, args }) => {
         const canvas = within(canvasElement);
 
         await step('Expect initial proportion to show on the button', async () => {
             const button = canvas.getByRole('button');
-            await expect(button).toHaveTextContent('Proportion 5.0% - 100.0%');
+            await expect(button).toHaveTextContent('TestPrefix 5.0% - 100.0%');
         });
 
         await step('Change min proportion and expect it to show on the button', async () => {
@@ -59,7 +68,7 @@ export const ProportionSelectorStory: StoryObj<ProportionSelectorDropdownProps> 
             await userEvent.clear(minInput);
             await userEvent.type(minInput, '10');
 
-            await waitFor(() => expect(button).toHaveTextContent('Proportion 10.0% - 100.0%'));
+            await waitFor(() => expect(button).toHaveTextContent('TestPrefix 10.0% - 100.0%'));
             await expect(args.setMinProportion).toHaveBeenCalledWith(0.1);
         });
     },
--- components/src/preact/components/proportion-selector-dropdown.tsx
+++ components/src/preact/components/proportion-selector-dropdown.tsx
@@ -3,18 +3,21 @@ import { type FunctionComponent } from 'preact';
 import { Dropdown } from './dropdown';
 import { ProportionSelector, type ProportionSelectorProps } from './proportion-selector';
 
-export type ProportionSelectorDropdownProps = ProportionSelectorProps;
+export type ProportionSelectorDropdownProps = ProportionSelectorProps & { labelPrefix?: string };
 
 export const ProportionSelectorDropdown: FunctionComponent<ProportionSelectorDropdownProps> = ({
     proportionInterval,
     setMinProportion,
     setMaxProportion,
+    labelPrefix = 'Proportion',
 }) => {
-    const label = `${(proportionInterval.min * 100).toFixed(1)}% - ${(proportionInterval.max * 100).toFixed(1)}%`;
+    const percentLabel = `${(proportionInterval.min * 100).toFixed(1)}% - ${(proportionInterval.max * 100).toFixed(1)}%`;
+
+    const width = 'w-[calc(1.5 * var(--tw-space-x-reverse) + 1.5 * var(--tw-space-x))]';
 
     return (
-        <div className='w-44'>
-            <Dropdown buttonTitle={`Proportion ${label}`} placement={'bottom-start'}>
+        <div className={width}>
+            <Dropdown buttonTitle={`${labelPrefix} ${percentLabel}`} placement={'bottom-start'}>
                 <ProportionSelector
                     proportionInterval={proportionInterval}
                     setMinProportion={setMinProportion}
