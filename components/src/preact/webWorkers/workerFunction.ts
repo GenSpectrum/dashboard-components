97bd6ad4d24e5330b40c11fdf746a9c6bb9d0193
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2024-11-19 16:52:54 +0100

    fix(components): propagate UserFacingErrors through the web worker (#528)
    Apparently, the concrete error type doesn't make it across the wire.
    Any error type that you send back from a web worker will be deserialized as a plain `Error`.

--- components/src/preact/webWorkers/workerFunction.ts
+++ components/src/preact/webWorkers/workerFunction.ts
@@ -1,14 +1,30 @@
+import { type ErrorWorkerStatus, type LoadingWorkerStatus, type SuccessWorkerStatus } from './useWebWorker';
+import { UserFacingError } from '../components/error-display';
+
 export async function workerFunction<R>(queryFunction: () => R) {
     try {
-        postMessage({ status: 'loading' });
+        postMessage({ status: 'loading' } satisfies LoadingWorkerStatus);
 
         const workerResponse = await queryFunction();
 
         postMessage({
             status: 'success',
             data: workerResponse,
-        });
+        } satisfies SuccessWorkerStatus<R>);
     } catch (error) {
-        postMessage({ status: 'error', error });
+        postMessage(
+            (error instanceof UserFacingError
+                ? {
+                      status: 'error',
+                      userFacing: true,
+                      headline: error.headline,
+                      error,
+                  }
+                : {
+                      status: 'error',
+                      userFacing: false,
+                      error: error instanceof Error ? error : new Error(`${error}`),
+                  }) satisfies ErrorWorkerStatus,
+        );
     }
 }
