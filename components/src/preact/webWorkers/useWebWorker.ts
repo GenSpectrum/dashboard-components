0564d1aa422061066964b119c201a949070ee6bd
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-11-19 15:05:46 +0100

    fix(components): propagate UserFacingErrors through the web worker
    Apparently, the concrete error type doesn't make it across the wire.
    Any error type that you send back from a web worker will be deserialized as a plain `Error`.

--- components/src/preact/webWorkers/useWebWorker.ts
+++ components/src/preact/webWorkers/useWebWorker.ts
@@ -1,31 +1,53 @@
 import { useEffect, useState } from 'preact/hooks';
 
+import { UserFacingError } from '../components/error-display';
+
+export type LoadingWorkerStatus = {
+    status: 'loading';
+};
+export type SuccessWorkerStatus<Response> = {
+    status: 'success';
+    data: Response;
+};
+export type ErrorWorkerStatus =
+    | {
+          status: 'error';
+          userFacing: false;
+          error: Error;
+      }
+    | {
+          status: 'error';
+          userFacing: true;
+          headline: string;
+          error: Error;
+      };
+export type WorkerStatus<Response> = LoadingWorkerStatus | SuccessWorkerStatus<Response> | ErrorWorkerStatus;
+
 export function useWebWorker<Request, Response>(messageToWorker: Request, worker: Worker) {
     const [data, setData] = useState<Response | undefined>(undefined);
     const [error, setError] = useState<Error | undefined>(undefined);
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-        worker.onmessage = (
-            event: MessageEvent<{
-                status: 'loading' | 'success' | 'error';
-                data?: Response;
-                error?: Error;
-            }>,
-        ) => {
-            const { status, data, error } = event.data;
+        worker.onmessage = (event: MessageEvent<WorkerStatus<Response>>) => {
+            const eventData = event.data;
+            const status = eventData.status;
 
             switch (status) {
                 case 'loading':
                     setIsLoading(true);
                     break;
                 case 'success':
-                    setData(data);
+                    setData(eventData.data);
                     setError(undefined);
                     setIsLoading(false);
                     break;
                 case 'error':
-                    setError(error);
+                    setError(
+                        eventData.userFacing
+                            ? new UserFacingError(eventData.headline, eventData.error.message)
+                            : eventData.error,
+                    );
                     setIsLoading(false);
                     break;
                 default:
