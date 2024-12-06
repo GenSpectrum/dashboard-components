572983e4a233856c1de5b235e98fe6b23e716c38
Fabian Engelniederhammer <fabian.engelniederhammer@tngtech.com>
2024-12-06 14:52:41 +0100

    gs-map

--- components/src/web-components/PreactLitAdapterWithGridJsStyles.tsx
+++ components/src/web-components/PreactLitAdapterWithGridJsStyles.tsx
@@ -1,4 +1,6 @@
 import gridJsStyle from 'gridjs/dist/theme/mermaid.css?inline';
+import leafletStyle from 'leaflet/dist/leaflet.css?inline';
+
 import { unsafeCSS } from 'lit';
 
 import { PreactLitAdapter } from './PreactLitAdapter';
@@ -6,7 +8,8 @@ import { PreactLitAdapter } from './PreactLitAdapter';
 import 'gridjs/dist/theme/mermaid.css';
 
 const gridJsElementCss = unsafeCSS(gridJsStyle);
+const leafletCss = unsafeCSS(leafletStyle);
 
 export abstract class PreactLitAdapterWithGridJsStyles extends PreactLitAdapter {
-    static override styles = [...PreactLitAdapter.styles, gridJsElementCss];
+    static override styles = [...PreactLitAdapter.styles, gridJsElementCss, leafletCss];
 }
