bf32a11e42ef39e74c4723bf5aa964ffcfbec859
Jonas Kellerer (TNG) <jonas.kellerer@tngtech.com>
2024-12-09 10:18:48 +0100

    test(components): add test for missing default value of lineage filter

--- components/src/preact/lineageFilter/lineage-filter.stories.tsx
+++ components/src/preact/lineageFilter/lineage-filter.stories.tsx
@@ -5,6 +5,7 @@ import { previewHandles } from '../../../.storybook/preview';
 import { AGGREGATED_ENDPOINT, LAPIS_URL } from '../../constants';
 import aggregatedData from '../../preact/lineageFilter/__mockData__/aggregated.json';
 import { LapisUrlContext } from '../LapisUrlContext';
+import { expectInvalidAttributesErrorMessage } from '../shared/stories/expectInvalidAttributesErrorMessage';
 
 const meta: Meta = {
     title: 'Input/LineageFilter',
@@ -31,6 +32,12 @@ const meta: Meta = {
             ],
         },
     },
+    args: {
+        lapisField: 'pangoLineage',
+        placeholderText: 'Enter lineage',
+        initialValue: '',
+        width: '100%',
+    },
 };
 
 export default meta;
@@ -46,10 +53,17 @@ export const Default: StoryObj<LineageFilterProps> = {
             />
         </LapisUrlContext.Provider>
     ),
+};
+
+export const WithNoLapisField: StoryObj<LineageFilterProps> = {
+    ...Default,
     args: {
-        lapisField: 'pangoLineage',
-        placeholderText: 'Enter lineage',
-        initialValue: '',
-        width: '100%',
+        ...Default.args,
+        lapisField: '',
+    },
+    play: async ({ canvasElement, step }) => {
+        step('expect error message', async () => {
+            await expectInvalidAttributesErrorMessage(canvasElement, 'String must contain at least 1 character(s)');
+        });
     },
 };
