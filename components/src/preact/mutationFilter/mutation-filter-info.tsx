5390aa7143f9c098219f3398bdbd8392765ec9ae
Fabian Engelniederhammer <92720311+fengelniederhammer@users.noreply.github.com>
2025-01-13 12:23:00 +0100

    feat(components): gs-mutation-filter: add quickstart guide to info text (#646)
    resolves #637

--- components/src/preact/mutationFilter/mutation-filter-info.tsx
+++ components/src/preact/mutationFilter/mutation-filter-info.tsx
@@ -1,6 +1,8 @@
 import { useContext } from 'preact/hooks';
+import { type FC } from 'react';
 
 import { isSingleSegmented } from '../../lapisApi/ReferenceGenome';
+import { type SequenceType } from '../../types';
 import { ReferenceGenomeContext } from '../ReferenceGenomeContext';
 import Info, { InfoHeadline1, InfoHeadline2, InfoParagraph } from '../components/info';
 
@@ -13,7 +15,38 @@ export const MutationFilterInfo = () => {
             <InfoHeadline1> Mutation Filter</InfoHeadline1>
             <InfoParagraph>This component allows you to filter for mutations at specific positions.</InfoParagraph>
 
-            <InfoHeadline2> Nucleotide Mutations and Insertions</InfoHeadline2>
+            <InfoHeadline2>Quickstart</InfoHeadline2>
+            <InfoParagraph>
+                <ul className='list-disc list-inside'>
+                    <li>
+                        Filter for nucleotide mutations:{' '}
+                        <ExampleMutation mutationType='substitution' sequenceType='nucleotide' />
+                    </li>
+                    <li>
+                        Filter for amino acid mutations:{' '}
+                        <ExampleMutation mutationType='insertion' sequenceType='nucleotide' />
+                    </li>
+                    <li>
+                        Filter for nucleotide insertions:{' '}
+                        <ExampleMutation mutationType='substitution' sequenceType='amino acid' />
+                    </li>
+                    <li>
+                        Filter for amino acid insertions:{' '}
+                        <ExampleMutation mutationType='insertion' sequenceType='amino acid' />
+                    </li>
+                </ul>
+            </InfoParagraph>
+            {!isSingleSegmented(referenceGenome) && (
+                <InfoParagraph>
+                    This organism has the following segments:{' '}
+                    {referenceGenome.nucleotideSequences.map((gene) => gene.name).join(', ')}.
+                </InfoParagraph>
+            )}
+            <InfoParagraph>
+                This organism has the following genes: {referenceGenome.genes.map((gene) => gene.name).join(', ')}.
+            </InfoParagraph>
+
+            <InfoHeadline2>Nucleotide Mutations and Insertions</InfoHeadline2>
             {isSingleSegmented(referenceGenome) ? (
                 <SingleSegmentedNucleotideMutationsInfo />
             ) : (
@@ -25,15 +58,12 @@ export const MutationFilterInfo = () => {
                 An amino acid mutation has the format <b>&lt;gene&gt;:&lt;position&gt;&lt;base&gt;</b> or
                 <b>&lt;gene&gt;:&lt;base_ref&gt;&lt;position&gt;&lt;base&gt;</b>. A <b>&lt;base&gt;</b> can be one of
                 the 20 amino acid codes. It can also be <b>-</b> for deletion and <b>X</b> for unknown. Example:{' '}
-                <b>E:57Q</b>.
+                <ExampleMutation mutationType='substitution' sequenceType='amino acid' />.
             </InfoParagraph>
             <InfoParagraph>
                 Insertions can be searched for in the same manner, they just need to have <b>ins_</b> appended to the
                 start of the mutation. Example: <b>ins_{firstGene}:31:N</b> would filter for sequences with an insertion
                 of N between positions 31 and 32 in the gene {firstGene}.
-            </InfoParagraph>
-            <InfoParagraph>
-                This organism has the following genes: {referenceGenome.genes.map((gene) => gene.name).join(', ')}.
             </InfoParagraph>
 
             <InfoHeadline2>Insertion Wildcards</InfoHeadline2>
@@ -106,12 +136,45 @@ const MultiSegmentedNucleotideMutationsInfo = () => {
             </InfoParagraph>
             <InfoParagraph>
                 Insertions can be searched for in the same manner, they just need to have <b>ins_</b> appended to the
-                start of the mutation. Example: <b>ins_{firstSegment}:10462:A</b>.
+                start of the mutation. Example: <ExampleMutation mutationType='insertion' sequenceType='nucleotide' />.
             </InfoParagraph>
-            <InfoParagraph>
-                This organism has the following segments:{' '}
-                {referenceGenome.nucleotideSequences.map((gene) => gene.name).join(', ')}.
-            </InfoParagraph>{' '}
         </>
     );
+};
+
+type ExampleMutationProps = {
+    sequenceType: SequenceType;
+    mutationType: 'substitution' | 'insertion';
+};
+
+const ExampleMutation: FC<ExampleMutationProps> = ({ sequenceType, mutationType }) => {
+    const referenceGenome = useContext(ReferenceGenomeContext);
+
+    const firstSegment = referenceGenome.nucleotideSequences[0].name;
+    const firstGene = referenceGenome.genes[0].name;
+
+    if (sequenceType === 'amino acid') {
+        switch (mutationType) {
+            case 'substitution':
+                return <b>{firstGene}:57Q</b>;
+            case 'insertion':
+                return <b>ins_{firstGene}:31:N</b>;
+        }
+    }
+
+    if (isSingleSegmented(referenceGenome)) {
+        switch (mutationType) {
+            case 'substitution':
+                return <b>23T</b>;
+            case 'insertion':
+                return <b>ins_1046:A</b>;
+        }
+    }
+
+    switch (mutationType) {
+        case 'substitution':
+            return <b>{firstSegment}:23T</b>;
+        case 'insertion':
+            return <b>ins_{firstSegment}:10462:A</b>;
+    }
 };
