5d1123debc1832d1402a15d9b8685e73fef293ca
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-03-28 09:55:33 +0100

    feat(components): remove tests

--- components/src/web-components/visualization/gs-genome-data-viewer.stories.ts
+++ components/src/web-components/visualization/gs-genome-data-viewer.stories.ts
@@ -1,4 +1,3 @@
-import { expect, waitFor } from '@storybook/test';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 
@@ -7,7 +6,6 @@ import '../gs-app';
 import { withComponentDocs } from '../../../.storybook/ComponentDocsBlock';
 import { LAPIS_URL } from '../../constants';
 import type { GenomeDataViewerProps } from '../../preact/genomeViewer/genome-data-viewer';
-import { withinShadowRoot } from '../withinShadowRoot.story';
 
 const codeExample = String.raw`
 <gs-genome-data-viewer
@@ -97,12 +95,5 @@ export const Default: StoryObj<Required<GenomeDataViewerProps>> = {
                 },
             ],
         },
-    },
-    play: async ({ canvasElement, step }) => {
-        const canvas = await withinShadowRoot(canvasElement, 'gs-genome-data-viewer');
-
-        await step('CDS and slider visible', async () => {
-            await waitFor(() => expect(canvas.getByText('Genome Data Viewer')).toBeVisible());
-        });
     },
 };
