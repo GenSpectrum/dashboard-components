811de27f7cec8d94dabad20ef899db73fc258b7d
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2025-04-28 22:11:52 +0200

    feat(components): remove quotes from CDS name

--- components/src/preact/genomeViewer/loadGff3.ts
+++ components/src/preact/genomeViewer/loadGff3.ts
@@ -60,6 +60,13 @@ function getAttributes(attributes: string): Map<string, string> {
     return attrMap;
 }
 
+function removeQuotes(input: string | undefined): string | undefined {
+    if (!input) {
+        return undefined;
+    }
+    return input.replace(/^['"](.*)['"]$/, '$1');
+}
+
 function getCDSMap(lines: string[], genome_type: string, geneMap: CDSMap): CDSMap {
     for (const line of lines) {
         if (line.startsWith('#') || line.trim() === '') {
@@ -73,7 +80,7 @@ function getCDSMap(lines: string[], genome_type: string, geneMap: CDSMap): CDSMa
 
         const [, , type, startStr, endStr, , , , attributes] = fields;
 
-        if (type.toLowerCase() !== genome_type.toLowerCase()) {
+        if (removeQuotes(type.toLowerCase()) !== genome_type.toLowerCase()) {
             continue;
         }
 
@@ -85,16 +92,16 @@ function getCDSMap(lines: string[], genome_type: string, geneMap: CDSMap): CDSMa
         }
 
         const attrPairs = getAttributes(attributes);
-        const label = attrPairs.get('Name') || attrPairs.get('gene') || attrPairs.get('gene_name');
+        const label = removeQuotes(attrPairs.get('Name') || attrPairs.get('gene') || attrPairs.get('gene_name'));
         if (!label) {
             throw new UserFacingError(
                 'Invalid gff3 source',
                 `No label found for feature: "${line}", must contain label in Name, gene, or gene_name attribute`,
             );
         }
-        const id = attrPairs.get('ID') || label;
+        const id = removeQuotes(attrPairs.get('ID') || label)!;
         if (attrPairs.get('Parent')) {
-            const parent = attrPairs.get('Parent');
+            const parent = removeQuotes(attrPairs.get('Parent'));
             if (parent && parent in geneMap) {
                 delete geneMap[parent];
             }
