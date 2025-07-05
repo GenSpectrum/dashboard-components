cfd0cdcd70c7902e4a75bb656bae622daea34057
Chaoran Chen <mail@chaoran-chen.de>
2025-07-05 16:50:21 +0200

    fix(components): accept lineage definitions without "aliases" key

--- components/src/lapisApi/LineageDefinition.ts
+++ components/src/lapisApi/LineageDefinition.ts
@@ -2,8 +2,14 @@ import z from 'zod';
 
 const lineage = z.object({
     parents: z.array(z.string()).optional(),
-    aliases: z.array(z.string()),
+    aliases: z.array(z.string()).default([]),
 });
 
-export const lineageDefinitionResponseSchema = z.record(lineage);
+// LAPIS' response does not include `aliases` as a key if no aliases are defined
+const lineageResponse = lineage.extend({
+    aliases: lineage.shape.aliases.optional(),
+});
+
+export const lineageDefinitionSchema = z.record(lineage);
+export const lineageDefinitionResponseSchema = z.record(lineageResponse);
 export type LineageDefinitionResponse = z.infer<typeof lineageDefinitionResponseSchema>;
