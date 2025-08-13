51f156f1831eec30d102bce71f5537fa226aeea6
Felix Hennig <mail@felixhennig.com>
2025-08-13 11:01:16 +0200

    Increase timeout (but why does it seem to be needed?)

--- components/src/preact/shared/stories/expectFinishedLoadingEvent.ts
+++ components/src/preact/shared/stories/expectFinishedLoadingEvent.ts
@@ -7,6 +7,6 @@ export function playThatExpectsFinishedLoadingEvent() {
         const componentFinishedLoadingListenerMock = fn();
         canvasElement.addEventListener(gsEventNames.componentFinishedLoading, componentFinishedLoadingListenerMock);
 
-        await waitFor(() => expect(componentFinishedLoadingListenerMock).toHaveBeenCalled());
+        await waitFor(() => expect(componentFinishedLoadingListenerMock).toHaveBeenCalled(), { timeout: 2000 });
     };
 }
