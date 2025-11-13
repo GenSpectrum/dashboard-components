a090b3eb7d5a775208813f9db51f976c310bc668
Felix Hennig <fhennig@users.noreply.github.com>
2025-11-13 10:28:14 +0000

    fix(components): fix overflow of multi lineage filter (#1036)

--- components/src/preact/components/resize-container.tsx
+++ components/src/preact/components/resize-container.tsx
@@ -4,11 +4,15 @@ import { forwardRef } from 'preact/compat';
 export type Size = {
     width: string;
     height?: string;
+    minHeight?: string;
 };
 
 export const ResizeContainer = forwardRef<HTMLDivElement, { size: Size; children: ComponentChildren }>(
     ({ size, children }, ref) => (
-        <div ref={ref} style={{ width: size.width, height: size.height, position: 'relative' }}>
+        <div
+            ref={ref}
+            style={{ width: size.width, height: size.height, minHeight: size.minHeight, position: 'relative' }}
+        >
             {children}
         </div>
     ),
